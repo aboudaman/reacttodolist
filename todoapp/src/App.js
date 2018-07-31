d4ddbac66b55a058771e89f61bb3dde79eefc443
aboudaman <adaman2000@gmail.com>
2018-07-31 13:07:33 -0400

    added events

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -2,55 +2,37 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
-class Counter extends React.Component {
-
-  state = {
-    counter: 0
-  }
-
-  render() {
-
-    return(
-      <div> 
-        <button type="button" className="btn btn-primary" onClick = {() => {this.setState({counter: this.state.counter + 1})} }>
-          Click Me
-        </button>
-        <p> clicked {this.state.counter} times </p>
-
-      </div>
-    )
-  }
-}
-
-
 class App extends Component {
 
   state = {
-    numbers: [
-      'Learn React',
-      'Learn Redux',
-      'Learn React Native',
-      'Create a web application'
-
-    ]
-
+    tasks: [
+      {title: 'Learn React', completed: false},
+      {title: 'Learn Redux', completed: false},
+      {title: 'Learn React Native', completed: false},
+      {title: 'Create a web application', completed: false}
+    ],
+    color: "",
+    checkBox: false
   }
 
   render() {
-    // const numbers = [
-    //   'Learn React',
-    //   'Learn Redux',
-    //   'Learn React Native',
-    //   'Create a web application'
-
-    // ];
 
     let num = 0;
 
-    const listItems = this.state.numbers.map((number) => 
-      <tr key={number.toString()}>
+    const listItems = this.state.tasks.map((task, i) => 
+      <tr key={task.title.toString()}>
         <td> {num++}</td>
-        <td>  {number} </td>
+        <td style={{color: task.completed? "green":""}}>  {task.title} </td>
+        <td><input type="checkbox" className="form-check-input" onChange={ () => {
+             //get copy of task object 
+              const { completed } = task
+              const [...taskCopy] = this.state.tasks //Get copy of task array
+              taskCopy[i] = {...task, completed: !completed}
+
+              //Update the array
+              this.setState({tasks: taskCopy})
+            }}/> 
+        </td>
         <td> pending </td>
         <td> edit | delete </td>
       </tr>
@@ -58,32 +40,22 @@ class App extends Component {
     return (
       <div className="row">
           <div className="col-sm"> 
-
             <h2> Task Manager </h2><br /><br />
-            <Counter /> <br />
             <input />
-            {/* <ul>
-              {listItems}
-            </ul> */}
             <table className="table">
               <thead>
                 <tr>
                   <th scope="col">#</th>
                   <th scope="col">Task</th>
+                  <th scope="col">Check Complete </th>
                   <th scope="col">Status</th>
                   <th scope="col">Admin</th>
                 </tr>
               </thead>
               <tbody>
-                {/* <tr>
-                  <th scope="row">1</th>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                  <td>@mdo</td>
-                </tr> */}
                 {listItems}
               </tbody>
-        </table>
+            </table>
         
       </div>
 
